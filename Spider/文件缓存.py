0fcb639897d7c4e17f030449fbb87d62eeed60d0
lighall <lighall@aliyun.com>
2020-09-13 16:04:19 +0800

    精简文件

--- Spider/文件缓存.py
+++
@@ -1,9 +0,0 @@
-import os
-
-dir = '/img'
-dir = os.path.abspath('.')+dir
-with open('cache.txt', 'w') as f:
-    for root, dirs, files in os.walk(dir, topdown=False):
-        for name in files:
-            f.write(os.path.join(root, name).replace(
-                os.path.abspath('.'), '').replace('\\', '/')+'\n')[1:]
